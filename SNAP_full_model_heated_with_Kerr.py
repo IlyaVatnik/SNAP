31194da938634b4ab885f2c9ecaec2c183769fa9
IlyaVatnik <ilya.vatnik@gmail.com>
2021-10-07 19:34:47 +0700

    add Kerr

--- SNAP_full_model_heated_with_Kerr.py
+++ SNAP_full_model_heated_with_Kerr.py
@@ -31,11 +31,12 @@ sigma=0.92*5.6e-8*1e-6 #W/mm**2/K**4 Stephan-Boltzman constant
 r=62.5e-3 #mm, fiber radius
 specific_heat_capacity=740 # J/kg/K
 density=2.2*1e-3*1e-3 # kg/mm**3
-
+refractive_index=1.45
 epsilon=1.5**2 #refractive index
-absorption_in_silica=3.27e-09*0 #absorption in silica, 1/mm
-thermal_optical_responce=1.25e9 # Hz/Celcium, detuning of the effective_ra
-
+absorption_in_silica=3.27e-09 #absorption in silica, 1/mm
+thermal_optical_responce=1.25e9  *0 # Hz/Celcium, detuning of the effective_ra
+hi_3=2.5e-16 #mm**2/ V
+wavelength_0=1550e-6 # mm
 
 """
 Sample parameters
@@ -51,7 +52,7 @@ absorption=50 #dB/m , absoprtion in the active core
 ESA_parameter=0.15 # Excitated state absorption parameter, from  [Guzman-Chavez AD, Barmenkov YO, Kir’yanov A V. Spectral dependence of the excited-state absorption of erbium in silica fiber within the 1.48–1.59μm range. Appl Phys Lett 2008;92:191111. https://doi.org/10.1063/1.2926671.]
 # thermal_expansion_coefficient=0.0107*r*1e3 #  nm/K, for effective radius variation
 
-transmission_from_taper_to_amplifier=0.1  # parts, betwee the taper and amplifier
+transmission_from_taper_to_amplifier=0.1 *0  # parts, betwee the taper and amplifier
 gain_small_signal=15
  # dB, gain of the amplifier guiding to the core
 P_sat=0.08 # W, saturation power for the amplifier
@@ -63,7 +64,7 @@ Properties of the input radiation
 """
 Pin=0.02 # W, power launched through the taper
 
-dv=-1e8 ## Hz, detuning of the pump from the center of the cold resonance 
+dv=1e8 ## Hz, detuning of the pump from the center of the cold resonance 
 d_dv=70e6
 dv_period=5e-4
 x_0=L/2 # point where the center of the mode is  and where taper is
@@ -84,14 +85,15 @@ def mode_distrib(x): # WGM mode distribution normilized as max(mode_distrib)=1
 grid parameters
 """
 
-t_max=2 # s
+t_max=1e-6 # s
 dv_max=100*(delta_0+delta_c)
 N_dv=200
 
 dx=0.05
 # dt = 1/delta_c/6 # also should be no less than dx**2/2/beta
-dt=1e-3 # s
+dt=5e-11 # s
 
+delta_t_to_save=dt*10 # s
 
 '''
 Internal parameters
@@ -99,14 +101,16 @@ Internal parameters
 
 N=int(L/dx) # number of numerical points along space axis
 x = np.linspace(0, L, N+1)
-u = np.zeros(N+1) # temperature
-U_0 = np.ones(N+1)*T0 #initial temperature distribution
+T_0 = np.ones(N+1)*T0 #initial temperature distribution
 
-
+frequency_0=c/wavelength_0
+mu=3*frequency_0*hi_3/8/refractive_index**2*2 # (11.19), p.174 frjm Gorodetsky
 
 Veff=integrate.quad(mode_distrib,-10,10)[0]*2*np.pi*r*2e-3 # effective volume of the WGM
 mode_distrib_array=np.array(list(map(mode_distrib,x)))
 mode_distrib_sum=np.sum(mode_distrib_array)
+
+n_steps_to_save=(delta_t_to_save//dt)
 
 beta = thermal_conductivity/specific_heat_capacity/density
 if dt>dx**2/beta/10:
@@ -126,41 +130,57 @@ core_heating_constant=alpha*ESA_parameter*transmission_from_taper_to_amplifier
 gain_small_signal_lin=10**(gain_small_signal/10)
 
 
+
+
 def solve_model(Pin,dv,t_max,a=0,T=np.ones(N+1)*T0):
     F=np.sqrt(4*Pin*delta_c/epsilon_0/epsilon/Veff)
     N_t=int(t_max/dt)
-    Indexes_to_save=[N_t]
     # Ensure that any list/tuple returned from f_ is wrapped as array
     rhs_thermal_array = lambda Pin,a,dv,T,T_averaged_over_mode, t: np.asarray(_rhs_thermal(Pin,a,dv,T,T_averaged_over_mode, t))
     t=0
+    time_array=[]
     T_array=[]
     a_array=[]
-    test=[]
-    TimeArray=np.linspace(0,dt*N_t,N_t+1)
+    T_averaged_dynamics=[]
+
     for n in range(N_t+1):
         t+=dt
         dv+=d_dv*np.sin(2*np.pi*t/dv_period)
-        # a=a+dt*_rhs_modal(F,a,u,t,dv)
         T_averaged_over_mode=np.sum(T*mode_distrib_array)/mode_distrib_sum
-        a=_analytical_step_for_WGM_amplitude(F,a,T_averaged_over_mode,dt,dv)
-        test.append(T_averaged_over_mode)
+        
+        # a=a+dt*_rhs_modal(F,a,T_averaged_over_mode,t,dv)
+        a=a+dt/6*Runge_Kutta_step(F,a,T_averaged_over_mode,t,dv)
+        # a=_analytical_step_for_WGM_amplitude(F,a,T_averaged_over_mode,dt,dv)
+                
+ 
+        
         if abs(a)>1e10:
             print('unstable simulation. Detuning is too large')
             TimeArray=np.linspace(0,dt*n,n)
             break
-        a_array.append(abs(a))
+        
         # if (n%100)==0:
         T+=dt*rhs_thermal_array(Pin,a,dv,T,T_averaged_over_mode, t)
         # test.append(heating_from_core(dv,L/2,du_average))
         # test.append(thermal_optical_responce*np.sum((u-T0)*mode_distrib_array/mode_distrib_sum))
-        if n in Indexes_to_save:
+        if (n%n_steps_to_save)==0:
+            time_array.append(t)
             T_array.append(T)
+            T_averaged_dynamics.append(T_averaged_over_mode)
+            a_array.append(abs(a))
         if (n%10000)==0:
             print('step ', n,' of ', N_t)
-    return TimeArray,a_array,T,test
+    return time_array,a_array,T,T_averaged_dynamics
 
-def _rhs_modal(F,a,T_averaged_over_mode,t,dv):
-    return 1j*F-a*(delta_c+delta_0)+a*1j*(thermal_optical_responce*(T_averaged_over_mode-T0)+dv)    
+def _rhs_modal(F,a,T_averaged_over_mode,t,dv):  # eq. (11.19), p 174 from Gorodetsky
+    return 1j*F-a*(delta_c+delta_0)+a*1j*(thermal_optical_responce*(T_averaged_over_mode-T0)+dv)+ 1j*mu*abs(a)**2
+
+def Runge_Kutta_step(F,a,T_averaged_over_mode,t,dv): # forward Runge_Kutta_ 4th order 
+    k1=_rhs_modal(F,a,T_averaged_over_mode,t,dv)
+    k2=_rhs_modal(F,a+k1*dt/2,T_averaged_over_mode,t+dt/2,dv)
+    k3=_rhs_modal(F,a+k2*dt/2,T_averaged_over_mode,t+dt/2,dv)
+    k4=_rhs_modal(F,a+k3*dt,T_averaged_over_mode,t+dt,dv)
+    return k1+k2+k3+k4
 
 def _analytical_step_for_WGM_amplitude(F,a,T_averaged_over_mode,t,dv):
     temp=1j*(thermal_optical_responce*(T_averaged_over_mode-T0)+dv)-delta_c-delta_0
@@ -177,6 +197,8 @@ def _rhs_thermal(Pin,a,dv,T,T_averaged_over_mode, t):
     rhs[N] = (beta/dx**2)*(2*T[N-1]  -
                            2*T[N]) +_heating_from_core(Pin,dv,x[N],T_averaged_over_mode)*theta+ _heating_from_WGM(a,x[N], t)*zeta - (T[N]-T0)*gamma -(T[N]+273)**4*delta+(T0+273)**4*delta #+ 2*dx*dudx(t)
     return rhs
+
+
 
 
 # def dsdt(): # derivative of the u at the left end 
@@ -236,7 +258,7 @@ time0=time.time()
 
 #%%
 
-# timeArray,a_array,T,test = solve_model(Pin,dv,t_max=3)
+# timeArray,a_array,T,T_averaged_dynamics = solve_model(Pin,dv,t_max=t_max)
 # fig=plt.figure(1)
 # x=x-L/2
 # plt.plot(x,T)
@@ -247,7 +269,7 @@ time0=time.time()
 # plt.xlabel('Time, s')
 # plt.ylabel('amplitude in the cavity, V/m')
 # plt.figure(3)
-# plt.plot(timeArray,test)
+# plt.plot(timeArray,T_averaged_dynamics)
 # plt.xlabel('Time, s')
 # plt.ylabel('Mode temperature, $^0C$')
 
@@ -268,7 +290,7 @@ plt.legend()
 plt.xlabel('detuning, Hz')
 plt.ylabel('Amplitude in the cavity, V/m')
 plt.title('Pin={:.3f},heat_from_mode={}, gain={:.2f}, transmission to amplifier={:.3f}'.format(Pin,bool(absorption_in_silica),gain_small_signal,transmission_from_taper_to_amplifier))
-# plt.savefig('Results\\Pin={:.3f},heat_from_mode={}, gain={:.2f}, transmission to amplifier={:.3f}.png'.format(Pin,bool(absorption_in_silica),gain_small_signal,transmission_from_taper_to_amplifier),dpi=300)
+plt.savefig('Results\\Pin={:.3f},heat_from_mode={}, gain={:.2f}, transmission to amplifier={:.3f}.png'.format(Pin,bool(absorption_in_silica),gain_small_signal,transmission_from_taper_to_amplifier),dpi=300)
 
 # fig=plt.figure(1)
 # for ind,t in enumerate(Times):
