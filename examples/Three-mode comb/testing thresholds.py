1c4683d1e6c829ea26a6df64645b8f6b96c89b5a
IlyaVatnik <ilya.vatnik@gmail.com>
2026-04-30 13:23:04 +0700

    Update testing thresholds.py

--- examples/Three-mode comb/testing thresholds.py
+++ examples/Three-mode comb/testing thresholds.py
@@ -7,18 +7,6 @@ import matplotlib.pyplot as plt
 
 #%%
 
-# wave_min,wave_max,res=lambda_0-0.01,lambda_0*(1+A/R_0*1.2), 1e-4
-
-# lambda_array=np.arange(wave_min,wave_max,res)
-
-# SNAP_1=SNAP_model.SNAP(z_dr,dr*1e3,lambda_array,lambda_0=lambda_0,res_width=1e-4,R_0=R_0)
-# SNAP_1.set_taper_params(absS=np.sqrt(0.8),phaseS=0.0,ReD=0.00,ImD_exc=0e-3,Csquared=0.00001)
-# fig=SNAP_1.plot_spectrogram(plot_ERV=True,scale='log')
-# SNAP_1.find_modes(plot_at_spectrogram=False)
-# #%%
-# Dint=SNAP_1.get_Dint(pump_mode)
-# plt.figure()
-# plt.plot(Dint)
 
 #%%
  
@@ -33,16 +21,16 @@ dr[mask1] = np.linspace(MaxRadVar, 0, np.sum(mask1))
 mask2= (z_dr <- h_width/2) & (z_dr>= -h_width/2 - length_of_steepness)
 dr[mask2] = np.linspace(0, MaxRadVar, np.sum(mask2))
 
-# cone=0.0000
-# dr+=cone*(z_dr-np.min(z_dr))*1e-3
-# dr+=np.random.random(len(z_dr))*0.0005*0
+cone=0.001
+dr+=cone*(z_dr-np.min(z_dr))*1e-3
+dr+=np.random.random(len(z_dr))*0.0005
 
 params = {
 # 'delta_0': 4e6, #total losses s^-1
 # 'delta_c': 2e6, # taper coupling, s^-1
 'Gamma': 3e6, # internal losses of the resonator, s^-1
 'Z_taper': 20, #   Taper position along z in microns
-'q0': 49, # Pump axial mode number (counting from 0)
+'q0': 2, # Pump axial mode number (counting from 0)
 'mu_max': 4, # maximum detuning that is taken into account
 'P_max': 1, # Desired power threshold
 'm_val': 354, # azimuthal number
@@ -58,9 +46,22 @@ params = {
 SNAP = SNAP_MI_threshold.SNAP_nonlinear_system(params,dim_space=2**10)
 SNAP.calculate_modes()
 min_threshold,mu_min,P_nl = SNAP.find_min_positive_threshold()
-
-SNAP.plot_modes_distribs([-1])
+#%%
+SNAP.plot_modes_distribs([20])
 #%%
 plt.figure()
-plt.plot(SNAP.z,SNAP.mode_distribs[:,0])
+plt.plot(SNAP.z,SNAP.mode_distribs[:,0]**2)
 # plt.plot(SNAP_1.x, SNAP_1.mode_distribs[0])
+
+#%%
+R_0=62.5
+lambda_0=1550
+wave_min,wave_max,res=lambda_0-0.01,lambda_0*(1+MaxRadVar/R_0*1.2), 1e-4
+
+lambda_array=np.arange(wave_min,wave_max,res)
+
+SNAP_1=SNAP_model.SNAP(z_dr,dr*1e3,lambda_array,lambda_0=lambda_0,res_width=1e-4,R_0=R_0)
+SNAP_1.set_taper_params(absS=np.sqrt(0.8),phaseS=0.0,ReD=0.00,ImD_exc=0e-3,Csquared=0.00001)
+fig=SNAP_1.plot_spectrogram(plot_ERV=True,scale='log')
+SNAP_1.find_modes(plot_at_spectrogram=False)
+#%%
